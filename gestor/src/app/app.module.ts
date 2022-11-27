cce03cc600167decf697afdff4220e3bc88a42b4
sebastian carrero <mugssebastian@gmail.com>
2022-11-27 17:39:15 -0500

    programando que la cantidad no sea menos a 0

--- gestor/src/app/app.module.ts
+++ gestor/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//modulos
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
+//componentes
 import { AppComponent } from './app.component';
 import { GastosComponent } from './componentes/gastos/gastos.component';
 import { IngresarPreComponent } from './componentes/ingresar-pre/ingresar-pre.component';
 import { IngresarGastosComponent } from './componentes/gastos/ingresar-gastos/ingresar-gastos.component';
+
 
 
 @NgModule({
@@ -17,7 +22,8 @@ import { IngresarGastosComponent } from './componentes/gastos/ingresar-gastos/in
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- gestor/src/app/componentes/ingresar-pre/ingresar-pre.component.ts
+++ gestor/src/app/componentes/ingresar-pre/ingresar-pre.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { PresupuestoService } from 'src/app/servicios/presupuesto.service';
 
 @Component({
   selector: 'app-ingresar-pre',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./ingresar-pre.component.css']
 })
 export class IngresarPreComponent {
+    cantidad: number;
+    cantidadIncorrecta: boolean;
+
+    constructor(private _presupuestoService: PresupuestoService, private router: Router){
+      this.cantidad = 0;
+      this.cantidadIncorrecta = false;
+    }
+
+    ngOnInit(): void{
+    }
+
+    agregar(){
+      if(this.cantidad > 0){
+        this.cantidadIncorrecta = false;
+        this._presupuestoService.presupuesto = this.cantidad;
+        this._presupuestoService.restante = this.cantidad;
+        this.router.navigate(['/gastos'])
+      }else{
+        this.cantidadIncorrecta = true;
+      }
+    }
 
 }
+
