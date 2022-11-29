172c583a2dd4d8779f94ece11223c90f75a98593
sebastian carrero <mugssebastian@gmail.com>
2022-11-28 21:35:27 -0500

    gasto a listar gasto

--- gestor/src/app/componentes/gastos/listar-gasto/listar-gasto.component.ts
+++ gestor/src/app/componentes/gastos/listar-gasto/listar-gasto.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PresupuestoService } from 'src/app/servicios/presupuesto.service';
 
 @Component({
   selector: 'app-listar-gasto',
   templateUrl: './listar-gasto.component.html',
   styleUrls: ['./listar-gasto.component.css']
 })
-export class ListarGastoComponent {
+export class ListarGastoComponent implements OnInit{
 
+    constructor(private _presupuestoService: PresupuestoService){
+      this,_presupuestoService.getGastos().subscribe(data => console.log(data));
+    }
+    ngOnInit(): void {
+        
+    }
 }
